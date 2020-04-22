5b79f4587be2441e9bb8986fa5bb30141ae0b1fd
Nirbhay Pherwani <pherwani37@gmail.com>
2020-04-22 09:19:12 -0400

    constants updated for public use

--- src/utility/constants.js
+++ src/utility/constants.js
@@ -1,8 +1,9 @@
 // all server and image constants.
-export const CLIENT_ID =
-  "53865530561-eoook3l9t27uvkec6561d3aolde6mims.apps.googleusercontent.com";
-export const GOOGLE_MAPS_API_KEY = "AIzaSyDZBgT-uZYXzTSkTJbiDcYT4D_XYsS8aUQ";
-export const AWS_SERVER_DOMAIN = "https://spothole.ml:5000";
+export const CLIENT_ID = "<YOUR GAPI CLIENT ID HERE>"; // GAPI CLIENT ID
+export const GOOGLE_MAPS_API_KEY = "<YOUR GOOGLE MAPS KEY HERE>"; // GOOGLE MAPS API KEY
+export const AWS_SERVER_DOMAIN = "<YOUR HTTPS FLASK SERVER URL HERE>"; // HTTPS FLASK SERVER URL
+export const HOST_NAME = "<YOUR DEPLOYED REACT APP URL HERE>"; // DEPLOYED REACT APP URL
+export const UA_LOGIN = "<YOUR UNAUTHORISED LOGIN PAGE URL HERE>"; // UNAUTHORIZED LOGIN PAGE URL
 export const GET_ALL_REPORTS = AWS_SERVER_DOMAIN + "/api/reports/all";
 export const GET_USER_REPORTS = AWS_SERVER_DOMAIN + "/api/reports";
 export const SUBMIT_REPORT_COMMENT =
@@ -13,7 +14,6 @@ export const UPLOAD_MEDIA = AWS_SERVER_DOMAIN + "/api/upload";
 export const DETECT_SINGLE_IMAGE = AWS_SERVER_DOMAIN + "/api/detect/single";
 export const POST_PROFILE_DATA = AWS_SERVER_DOMAIN + "/api/profile/update";
 export const VALIDATE_USER = AWS_SERVER_DOMAIN + "/api/user/validate";
-export const HOST_NAME = "http://nirbhay.me/spothole";
 export const AUTHORITY_CHAT_LOGO = "https://i.ibb.co/Nry0dDp/image.png";
 export const ROUTE_ICON_IMAGE =
   "https://cdn4.iconfinder.com/data/icons/car-accident-2/512/pothole-road-hole-danger-street-accident-512.png";
@@ -23,4 +23,3 @@ export const ROUTE_ICON_IMAGE_MEDIUM =
   "https://i.ibb.co/RC2LmDh/output-onlinepngtools-1.png";
 export const ROUTE_ICON_IMAGE_HIGH =
   "https://i.ibb.co/kMZV43H/output-onlinepngtools.png";
-export const UA_LOGIN = "https://nirbhay.me/spothole/UALogin";
